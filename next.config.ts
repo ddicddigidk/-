2bb80a7b74579f9ac266d6c6ba0dceaa73f86511
ddicddigidk <ddicddigi@dankook.ac.kr>
2026-07-30 13:06:54 +0900

    Fix: 배포 경로 CSS 깨짐 수정

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  // 📌 아래 두 줄을 새로 추가합니다.
+  basePath: "/my-website",
+  assetPrefix: "/my-website/",
 };
 
 export default nextConfig;
